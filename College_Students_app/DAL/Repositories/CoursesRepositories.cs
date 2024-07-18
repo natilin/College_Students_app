301ab7a9f6596f4eefa89b9efea4ccd6650533da
Nati Lintzer <nlinzer@gmail.com>
2024-07-18 17:46:26 +0300

    מעדכן שינויים

--- College_Students_app/DAL/Repositories/CoursesRepositories.cs
+++ College_Students_app/DAL/Repositories/CoursesRepositories.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +16,54 @@ namespace College_Students_app.DAL.Repositories
             _dbContext = dbContext;
         }
 
+    } List<string> GetCoursesListToRegestery(int ID)
+    {
+        string query = @"select DISTINCT c.Name
+            from Courses c
+            INNER JOIN Cycles ON c.Course_ID = Cycles.Course_ID
+            INNER JOIN Cycles_Student ON Cycles.Cycle_ID = Cycles_Student.Cycle_ID
+            WHERE Cycles_Student.Cycle_ID != @ID";
     }
+
+
+    bool RegisteryToCycle(int studentID, int cycleID)
+    {
+
+        // משתמש בטרנזקציה כדי לוודא שלא ייתכן מצב שמתמש נרשם לקורס אבל לא התעדכן המחיר ברשימות או ההיפך
+        string query = @"BEGIN TRANSACTION
+	        DECLARE @success int;
+	        BEGIN TRY
+		
+		        INSERT INTO Cycles_Student
+		        VALUES (@Student_ID, @Cycle_ID);
+
+		        DECLARE @Cycle_Price int;
+
+		        SELECT @Cycle_Price = Price 
+		        FROM Cycles
+		        WHERE Cycle_ID =  @Cycle_ID
+
+		        UPDATE Students 
+		        SET Total_Price = Total_Price + @Cycle_Price
+		        WHERE Student_ID = @Student_ID
+
+		        SET @success = 1;
+		        COMMIT TRANSACTION
+
+	        END TRY
+
+	        BEGIN CATCH
+		        ROLLBACK TRANSACTION;
+		        SET @success = 0;
+	        END CATCH
+
+        SELECT @success";
+
+        SqlParameter[] parameters = {
+                new SqlParameter("@Student_ID", studentID),
+                new SqlParameter("@Cycle_ID", cycleID),
+ 
+            };
+    }
+
 }
